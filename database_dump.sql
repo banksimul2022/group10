3e97a6d6721502dd892539a6381ee4579ca36e91
OsmanAkbaba <akbaba.98@hotmail.com>
2022-04-04 12:27:02 +0300

    päivitetty tietokanta

--- database_dump.sql
+++ database_dump.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: pankkiautomaatti
 -- ------------------------------------------------------
@@ -28,7 +28,7 @@ CREATE TABLE `asiakas` (
   `Osoite` varchar(45) NOT NULL,
   `puhNum` varchar(45) NOT NULL,
   PRIMARY KEY (`idAsiakas`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `asiakas` (
 
 LOCK TABLES `asiakas` WRITE;
 /*!40000 ALTER TABLE `asiakas` DISABLE KEYS */;
+INSERT INTO `asiakas` VALUES (1,'Jussi Lappalainen','Lekatie 21','040123456'),(2,'Hannu Hanhi','Poratie 5','040234567'),(3,'Perttu Pihamaa','Poratie 33','0403456786'),(4,'Matti Nykänen','Lekatie 10','0404567891');
 /*!40000 ALTER TABLE `asiakas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -105,11 +106,11 @@ DROP TABLE IF EXISTS `tili`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tili` (
-  `idTili` int NOT NULL,
+  `idTili` int NOT NULL AUTO_INCREMENT,
   `Tilinumero` varchar(45) NOT NULL,
   `Saldo` int NOT NULL,
   PRIMARY KEY (`idTili`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,6 +119,7 @@ CREATE TABLE `tili` (
 
 LOCK TABLES `tili` WRITE;
 /*!40000 ALTER TABLE `tili` DISABLE KEYS */;
+INSERT INTO `tili` VALUES (1,'01234',2000),(2,'02345',2001),(3,'03456',2002),(4,'04567',2003);
 /*!40000 ALTER TABLE `tili` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,7 +131,7 @@ DROP TABLE IF EXISTS `tilitapahtumat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tilitapahtumat` (
-  `idTilitapahtumat` int NOT NULL,
+  `idTilitapahtumat` int NOT NULL AUTO_INCREMENT,
   `Tilinumero` varchar(45) NOT NULL,
   `KortinNumero` varchar(45) NOT NULL,
   `PVM` date NOT NULL,
@@ -163,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-30 12:17:57
+-- Dump completed on 2022-03-31 12:18:21
