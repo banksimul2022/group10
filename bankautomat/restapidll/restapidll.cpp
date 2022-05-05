098ab71ed54ff50fc2fc7a75831ecf64bd1856fc
Lapeee <lauri.ilkka@gmail.com>
2022-05-05 11:11:42 +0300

    ???

--- bankautomat/restapidll/restapidll.cpp
+++ bankautomat/restapidll/restapidll.cpp
@@ -10,31 +10,34 @@ Restapidll::~Restapidll()
 
 }
 
-void Restapidll::getSaldo(QString id)
+void Restapidll::getSaldo(QString)
 {
-    QString site_url="http://localhost:3000/saldo/" + id + "/";
+    QString site_url="http://localhost:3000/tili/1";
     QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    //QByteArray MyToken="Bearer "+response_data;
+    request.setRawHeader(QByteArray("Authorization"),(token));
 
     getSaldoManager = new QNetworkAccessManager(this);
-
-    connect(getSaldoManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(getSaldoSlot(QNetworkReply*)));
-
+    connect(getSaldoManager, SIGNAL(finished(QNetworkReply*)),this, SLOT(getSaldoSlot(QNetworkReply*)));
     reply = getSaldoManager->get(request);
 }
 
 
 void Restapidll::getSaldoSlot(QNetworkReply* reply)
 {
-    response_data=reply->readAll();
-    qDebug()<<"DATA : "+response_data;
-
+    QByteArray response_data=reply->readAll();
     QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
     QJsonArray json_array = json_doc.array();
-    QString saldo;
+    QString Saldo;
     foreach (const QJsonValue &value, json_array) {
-       QJsonObject json_obj = value.toObject();
-       saldo+=QString::number(json_obj["Saldo"].toInt());
+        QJsonObject json_obj = value.toObject();
+        Saldo+=QString::number(json_obj["Saldo"].toInt())+"\r";
+        emit saldoToExe(Saldo);
     }
+
+    qDebug()<<Saldo;
     reply->deleteLater();
     getSaldoManager->deleteLater();
 }
@@ -61,20 +64,158 @@ void Restapidll::loginSlot(QNetworkReply *reply)
         else if(response_data =="false"){
             response_data = "PIN-koodi väärin";
         }
+        token="Bearer "+response_data; //lisätty
         emit loginSignal(response_data);
         qDebug()<<response_data;
         reply->deleteLater();
         loginManager->deleteLater();
 }
 
+void Restapidll::nosto(QString Summa)
+{
+    QJsonObject jsonObj;
+    jsonObj.insert("Saldo", Summa);
+    QString site_url="http://localhost:3000/tili/1";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    nostoManager = new QNetworkAccessManager(this);
+    connect(nostoManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(nostoSlot(QNetworkReply*)));
+    reply = nostoManager->post(request, QJsonDocument(jsonObj).toJson());
+}
+
 void Restapidll::nostoSlot(QNetworkReply *reply)
 {
-
+    emit summaToExe(response_data);
+    response_data=reply->readAll();
+    qDebug()<<response_data;
+    reply->deleteLater();
+    nostoManager->deleteLater();
 }
+
+void Restapidll::getTT10(QString)
+{
+    QString site_url="http://localhost:3000/tilitapahtumat/1";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    //QByteArray MyToken="Bearer "+response_data;
+    request.setRawHeader(QByteArray("Authorization"),(token));
+
+    getTT10Manager = new QNetworkAccessManager(this);
+    connect(getTT10Manager, SIGNAL(finished(QNetworkReply*)),this, SLOT(getTT10Slot(QNetworkReply*)));
+    reply = getTT10Manager->get(request);
+}
+
 
 void Restapidll::getTT10Slot(QNetworkReply *reply)
 {
+    QByteArray response_data=reply->readAll();
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+    QString tilitapahtumat;
+    foreach (const QJsonValue &value, json_array) {
+        QJsonObject json_obj = value.toObject();
+        tilitapahtumat+=QString::number(json_obj["idTilitapahtumat"].toInt())+","+json_obj["tilinumero"].toString()+","+json_obj["KortinNumero"].toString()+","+json_obj["PVM"].toString()+","+json_obj["Tapahtuma"].toString()+","+QString::number(json_obj["Summa"].toInt())+","+QString::number(json_obj["idKortti"].toInt())+","+QString::number(json_obj["idTili"].toInt())+"\r";
+        emit TT10ToExe(tilitapahtumat);
+    }
 
+    qDebug()<<tilitapahtumat;
+    reply->deleteLater();
+    getTT10Manager->deleteLater();
 }
 
 
+void Restapidll::getNimi(QString)
+{
+    QString site_url="http://localhost:3000/asiakas/nimi/1";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    //QByteArray MyToken="Bearer "+token;
+    request.setRawHeader(QByteArray("Authorization"),(token));
+
+    getNimiManager = new QNetworkAccessManager(this);
+    connect(getNimiManager, SIGNAL(finished(QNetworkReply*)),this, SLOT(getNimiSlot(QNetworkReply*)));
+    reply = getNimiManager->get(request);
+}
+
+void Restapidll::getNimiSlot(QNetworkReply *reply)
+{
+    QByteArray response_data=reply->readAll();
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+    QString Nimi;
+    foreach (const QJsonValue &value, json_array) {
+        QJsonObject json_obj = value.toObject();
+        Nimi+=json_obj["nimi"].toString()+"\r";
+        emit nimiToExe(Nimi);
+    }
+
+    qDebug()<<Nimi;
+    reply->deleteLater();
+    getNimiManager->deleteLater();
+}
+
+void Restapidll::getTT5(QString)
+{
+    QString site_url="http://localhost:3000/tilitapahtumat/5tapahtumaa/1";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    //QByteArray MyToken="Bearer "+response_data;
+    request.setRawHeader(QByteArray("Authorization"),(token));
+
+    getTT5Manager = new QNetworkAccessManager(this);
+    connect(getTT5Manager, SIGNAL(finished(QNetworkReply*)),this, SLOT(getTT5Slot(QNetworkReply*)));
+    reply = getTT5Manager->get(request);
+}
+
+
+void Restapidll::getTT5Slot(QNetworkReply *reply)
+{
+    QByteArray response_data=reply->readAll();
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+    QString tilitapahtumat5;
+    foreach (const QJsonValue &value, json_array) {
+        QJsonObject json_obj = value.toObject();
+        tilitapahtumat5+=QString::number(json_obj["idTilitapahtumat"].toInt())+","+json_obj["tilinumero"].toString()+","+json_obj["KortinNumero"].toString()+","+json_obj["PVM"].toString()+","+json_obj["Tapahtuma"].toString()+","+QString::number(json_obj["Summa"].toInt())+","+QString::number(json_obj["idKortti"].toInt())+","+QString::number(json_obj["idTili"].toInt())+"\r";
+        emit TT5ToExe(tilitapahtumat5);
+    }
+
+    qDebug()<<tilitapahtumat5;
+    reply->deleteLater();
+    getTT5Manager->deleteLater();
+}
+
+
+void Restapidll::getTT5eka(QString)
+{
+    QString site_url="http://localhost:3000/tilitapahtumat/5Ekaatapahtumaa/1";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    //QByteArray MyToken="Bearer "+response_data;
+    request.setRawHeader(QByteArray("Authorization"),(token));
+
+    getTT5EkaManager = new QNetworkAccessManager(this);
+    connect(getTT5EkaManager, SIGNAL(finished(QNetworkReply*)),this, SLOT(getTT5ekaSlot(QNetworkReply*)));
+    reply = getTT5EkaManager->get(request);
+}
+
+void Restapidll::getTT5ekaSlot(QNetworkReply *reply)
+{
+    QByteArray response_data=reply->readAll();
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+    QString tilitapahtumat5ekaa;
+    foreach (const QJsonValue &value, json_array) {
+        QJsonObject json_obj = value.toObject();
+        tilitapahtumat5ekaa+=QString::number(json_obj["idTilitapahtumat"].toInt())+","+json_obj["tilinumero"].toString()+","+json_obj["KortinNumero"].toString()+","+json_obj["PVM"].toString()+","+json_obj["Tapahtuma"].toString()+","+QString::number(json_obj["Summa"].toInt())+","+QString::number(json_obj["idKortti"].toInt())+","+QString::number(json_obj["idTili"].toInt())+"\r";
+        emit TT5ekaToExe(tilitapahtumat5ekaa);
+    }
+
+    qDebug()<<tilitapahtumat5ekaa;
+    reply->deleteLater();
+    getTT5EkaManager->deleteLater();
+}
--- bankautomat/restapidll/restapidll.h
+++ bankautomat/restapidll/restapidll.h
@@ -15,25 +15,41 @@ class RESTAPIDLL_EXPORT Restapidll : public QObject
 public:
     Restapidll(QObject * parent = nullptr);
     ~Restapidll();
-    void getSaldo(QString id);
-    void login(QString PIN, QString KortinNumero);
-    void nosto(QString, QString, QString);
+    void getSaldo(QString);
+    void login(QString KortinNumero, QString PIN);
+    void nosto(QString Summa); //muokattu
     void getTT10(QString);
+    void getTT5(QString); //uusi
+    void getTT5eka(QString); //uusi
+    void getNimi(QString); //uusi
 
 signals:
     void loginSignal(QString);
+    void saldoToExe(QString);
+    void summaToExe(QString); //uusi
+    void TT10ToExe(QString); //uusi
+    void TT5ToExe(QString); //uusi
+    void TT5ekaToExe(QString); //uusi
+    void nimiToExe(QString); //uusi
 private slots:
     void getSaldoSlot(QNetworkReply *reply);
     void loginSlot(QNetworkReply *reply);
     void nostoSlot(QNetworkReply *reply);
     void getTT10Slot(QNetworkReply *reply);
+    void getTT5Slot(QNetworkReply *reply); //uusi
+    void getTT5ekaSlot(QNetworkReply *reply); //uusi
+    void getNimiSlot(QNetworkReply *reply); //uusi
 private:
     QNetworkAccessManager *getSaldoManager;
     QNetworkAccessManager *loginManager;
     QNetworkAccessManager *nostoManager;
     QNetworkAccessManager *getTT10Manager;
+    QNetworkAccessManager *getTT5Manager; //uusi
+    QNetworkAccessManager *getTT5EkaManager; //uusi
+    QNetworkAccessManager *getNimiManager; //uusi
     QNetworkReply *reply;
     QByteArray response_data;
+    QByteArray token;
 
 
 };
